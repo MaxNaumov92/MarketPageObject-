6580793a654118f29211b5c76ed495444a873126
Shizanutik321 <craft.25@mail.ru>
2021-04-28 13:51:04 +0300

    add locators for login page and create test_main_page file

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,15 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    # Локаторы для "Войти"
+    LOGIN_EMAIL_HOLDER = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD_HOLDER = (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_ENTER_BUTTON = (By.CSS_SELECTOR , "[name=login_submit]")
+    # Локаторы для "Зарегистрироваться"
+    REG_EMAIL_HOLDER = (By.CSS_SELECTOR, "#id_registration-email")
+    REG_PASSWORD_HOLDER = (By.CSS_SELECTOR, "#id_registration-password1")
+    REG_PASSWORD_APPROVE_HOLDER = (By.CSS_SELECTOR, "#id_registration-password2")
+    REG_REGISTRATION_BUTTON = (By.CSS_SELECTOR, "[name=registration_submit]")
