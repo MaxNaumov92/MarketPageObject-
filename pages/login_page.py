2540f4163be3fceb64d8ae626c0b9299370c1d6d
Shizanutik321 <craft.25@mail.ru>
2021-04-28 14:27:28 +0300

    small refactor changes

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-
+from selenium.webdriver.common.by import By
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -12,19 +12,19 @@ class LoginPage(BasePage):
         assert "login" in self.browser.current_url
 
     def should_be_login_form(self):
-        assert self.browser.element_is_presented(*LoginPageLocators.LOGIN_EMAIL_HOLDER), \
-            "ENTER-EMAIl holder is presented"
-        assert self.browser.element_is_presented(*LoginPageLocators.LOGIN_PASSWORD_HOLDER), \
-            "ENTER-PASSWORD holder is presented"
+        assert self.browser.element_is_enabled(*LoginPageLocators.LOGIN_EMAIL_HOLDER), \
+            "ENTER-EMAIl holder is not enabled"
+        assert self.browser.element_is_enabled(*LoginPageLocators.LOGIN_PASSWORD_HOLDER), \
+            "ENTER-PASSWORD holder is not enabled"
         assert self.browser.element_is_clickable(*LoginPageLocators.LOGIN_ENTER_BUTTON), \
-            "ENTER button is clickable"
+            "ENTER button is not clickable"
 
     def should_be_register_form(self):
-        assert self.browser.element_is_presented(*LoginPageLocators.REG_EMAIL_HOLDER), \
-            "REG-EMAIL holder is presented"
-        assert self.browser.element_is_presented(*LoginPageLocators.REG_PASSWORD_HOLDER), \
-            "REG-PASSWORD holder is presented"
-        assert self.browser.element_is_presented(*LoginPageLocators.REG_PASSWORD_APPROVE_HOLDER), \
-            "REG-PASSWORD-APPROVE holder is presented"
+        assert self.browser.element_is_enabled(*LoginPageLocators.REG_EMAIL_HOLDER), \
+            "REG-EMAIL holder is not presented"
+        assert self.browser.element_is_enabled(*LoginPageLocators.REG_PASSWORD_HOLDER), \
+            "REG-PASSWORD holder is not presented"
+        assert self.browser.element_is_enabled(*LoginPageLocators.REG_PASSWORD_APPROVE_HOLDER), \
+            "REG-PASSWORD-APPROVE holder is not presented"
         assert self.browser.element_is_clickable(*LoginPageLocators.REG_REGISTRATION_BUTTON), \
-            "REG-BUTTON is clickable"
+            "REG-BUTTON is not clickable"
