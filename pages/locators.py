5728e6c1ff780c78aa9c210ce8f78d46409b0aac
Shizanutik321 <craft.25@mail.ru>
2021-04-28 17:42:33 +0300

    add locators for product page

--- pages/locators.py
+++ pages/locators.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
+    # Локатор для перехода по ссылке Войти или зарегистрироваться
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
@@ -15,3 +16,11 @@ class LoginPageLocators():
     REG_PASSWORD_HOLDER = (By.CSS_SELECTOR, "#id_registration-password1")
     REG_PASSWORD_APPROVE_HOLDER = (By.CSS_SELECTOR, "#id_registration-password2")
     REG_REGISTRATION_BUTTON = (By.CSS_SELECTOR, "[name=registration_submit]")
+
+
+class ProductPageLocators():
+    ADD_TO_CARD_BUTTON = (By.CSS_SELECTOR, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
+    PRODUCT_NAME_ADD_TO_CARD_MESSAGE = (By.CSS_SELECTOR, "#messages .alertinner strong")
+    PRODUCT_PRICE_ADD_TO_CARD_MESSAGE = (By.CSS_SELECTOR, "#messages  .alert.alert-safe.alert-noicon.alert-info strong")
---
+++ pages/product_page.py
@@ -0,0 +1,6 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def
