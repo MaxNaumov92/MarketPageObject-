2ec16461ac46f9a7944d908b3e18330ff38e7d84
Shizanutik321 <craft.25@mail.ru>
2021-04-28 14:34:04 +0300

    remove browser from login_page, this method need just 'self'

--- pages/login_page.py
+++ pages/login_page.py
@@ -12,19 +12,19 @@ class LoginPage(BasePage):
         assert "login" in self.browser.current_url
 
     def should_be_login_form(self):
-        assert self.browser.element_is_enabled(*LoginPageLocators.LOGIN_EMAIL_HOLDER), \
-            "ENTER-EMAIl holder is not enabled"
-        assert self.browser.element_is_enabled(*LoginPageLocators.LOGIN_PASSWORD_HOLDER), \
-            "ENTER-PASSWORD holder is not enabled"
-        assert self.browser.element_is_clickable(*LoginPageLocators.LOGIN_ENTER_BUTTON), \
-            "ENTER button is not clickable"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL_HOLDER), \
+            "ENTER-EMAIl holder is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD_HOLDER), \
+            "ENTER-PASSWORD holder is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_ENTER_BUTTON), \
+            "ENTER button is not presented"
 
     def should_be_register_form(self):
-        assert self.browser.element_is_enabled(*LoginPageLocators.REG_EMAIL_HOLDER), \
+        assert self.is_element_present(*LoginPageLocators.REG_EMAIL_HOLDER), \
             "REG-EMAIL holder is not presented"
-        assert self.browser.element_is_enabled(*LoginPageLocators.REG_PASSWORD_HOLDER), \
+        assert self.is_element_present(*LoginPageLocators.REG_PASSWORD_HOLDER), \
             "REG-PASSWORD holder is not presented"
-        assert self.browser.element_is_enabled(*LoginPageLocators.REG_PASSWORD_APPROVE_HOLDER), \
+        assert self.is_element_present(*LoginPageLocators.REG_PASSWORD_APPROVE_HOLDER), \
             "REG-PASSWORD-APPROVE holder is not presented"
-        assert self.browser.element_is_clickable(*LoginPageLocators.REG_REGISTRATION_BUTTON), \
-            "REG-BUTTON is not clickable"
+        assert self.is_element_present(*LoginPageLocators.REG_REGISTRATION_BUTTON), \
+            "REG-BUTTON is not presented"
