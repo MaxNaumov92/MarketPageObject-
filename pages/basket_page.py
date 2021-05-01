f8a1d2a0f5f0415eed11064e0d504bc3b98fb9cb
Shizanutik321 <craft.25@mail.ru>
2021-05-01 12:57:16 +0300

    refactor basket check methods

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,23 +1,14 @@
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import NoSuchElementException
 from .base_page import BasePage
+from .locators import BasketPageLocators
 
 
 class BasketPage(BasePage):
-    def basket_has_no_products_check(self, how, what, timeout=4):
-        # Проверка пустой корзины (timeout)
-        try:
-            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return True
-        return False
+    # Ожидаем, что есть текст о том что корзина пуста
+    def basket_has_empty_basket_text_check(self):
+        assert self.is_element_present(*BasketPageLocators.CARD_CONTENT_HOLDER), \
+            "Message 'basket is empty' is not presented"
 
-    def basket_has_empty_basket_text_check(self, how, what):
-        # Проверка наличия текста о пустой корзине
-        try:
-            self.browser.find_element(how, what)
-        except (NoSuchElementException):
-            return False
-        return True
+    # Ожидаем, что в корзине нет товаров
+    def basket_has_no_products_check(self):
+        assert self.is_not_element_present(*BasketPageLocators.CARD_CONTENT_NUMBER_OF_PRODUCTS), \
+            "Your basket is not empty!"
